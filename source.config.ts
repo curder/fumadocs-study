a7b71c697ed7411922fe9df754b874872a81b1cb
curder <q.curder@gmail.com>
2025-04-16 18:49:10 +0800

    feat: 添加代码高亮配置并创建相关文档
    在 `source.config.ts` 中添加了 `rehypeCodeOptions` 配置，支持代码块的懒加载、实验性 JS 引擎以及多种语言的高亮显示。同时创建了 `code.mdx` 文档，详细介绍了代码标题和内联代码的配置及使用方式

--- source.config.ts
+++ source.config.ts
@@ -1,12 +1,23 @@
-import { defineDocs, defineConfig } from 'fumadocs-mdx/config';
+import { defineDocs, defineConfig } from "fumadocs-mdx/config";
+import { InlineTOC } from "fumadocs-ui/components/inline-toc";
 
 // Options: https://fumadocs.vercel.app/docs/mdx/collections#define-docs
 export const docs = defineDocs({
-  dir: 'content/docs',
+  dir: "content/docs",
 });
 
 export default defineConfig({
   mdxOptions: {
     // MDX options
+    rehypeCodeOptions: {
+      lazy: true,
+      experimentalJSEngine: true,
+      langs: ["ts", "js", "html", "tsx", "mdx"],
+      inline: "tailing-curly-colon",
+      themes: {
+        light: "github-light",
+        dark: "github-dark",
+      },
+    },
   },
 });
