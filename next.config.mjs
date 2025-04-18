6bae93448622cab5a0d64f89dcd53fa51e788afc
curder <q.curder@gmail.com>
2025-04-18 18:54:25 +0800

    ci: 添加 GitHub Pages 发布工作流和 Node.js 设置
    添加了新的 GitHub Actions 工作流，用于在推送到主分支时自动构建并发布到 GitHub Pages。同时，新增了 Node.js 设置和依赖缓存的工作流，以提高构建效率。

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,11 @@
-import { createMDX } from 'fumadocs-mdx/next';
+import { createMDX } from "fumadocs-mdx/next";
 
 const withMDX = createMDX();
 
 /** @type {import('next').NextConfig} */
 const config = {
+  basePath: "/fumadocs-study",
+  output: "export",
   reactStrictMode: true,
 };
 
