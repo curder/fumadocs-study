f54358cd32277e7151690e41976da30e9f6ce892
curder <q.curder@gmail.com>
2025-04-18 11:40:52 +0800

    feat: 更新手风琴组件文档，添加属性表格支持

--- content/docs/props.ts
+++ content/docs/props.ts
@@ -2,9 +2,20 @@ import type {
     ComponentPropsWithoutRef,
 } from 'react';
 import {TypeTable} from "fumadocs-ui/components/type-table";
+import {Accordion, Accordions} from "fumadocs-ui/components/accordion";
 
 export type TypeTableProps = ComponentPropsWithoutRef<typeof TypeTable>;
 
 export type TypeTableObjectTypeProps = ComponentPropsWithoutRef<
     typeof TypeTable
 >['type'][string];
+
+export type AccordionsProps = Omit<
+    ComponentPropsWithoutRef<typeof Accordions>,
+    keyof ComponentPropsWithoutRef<'div'> | 'value' | 'onValueChange'
+>;
+
+export type AccordionProps = Omit<
+    ComponentPropsWithoutRef<typeof Accordion>,
+    keyof ComponentPropsWithoutRef<'div'>
+>;
